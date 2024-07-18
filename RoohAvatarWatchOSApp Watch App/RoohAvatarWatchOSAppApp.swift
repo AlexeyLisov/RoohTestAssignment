438f2656dc20ea06bca33c459125d069442c761c
Alexey Lisov <lisov.alex@gmail.com>
2024-07-18 19:00:33 +0100

    Initila commit

---
+++ RoohAvatarWatchOSApp Watch App/RoohAvatarWatchOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoohAvatarWatchOSAppApp.swift
+//  RoohAvatarWatchOSApp Watch App
+//
+//  Created by Alexey Lisov on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoohAvatarWatchOSApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
